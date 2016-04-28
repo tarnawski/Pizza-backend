5d630b2e6f895b97b7b59e5d52802a4ff94673c1
ttarnawski <ttranawski@pgs-soft.com>
2016-04-29 00:20:38 +0200

    Add function to send SMS

--- src/PizzaBundle/Notification/Strategies/SmsStrategy.php
+++ src/PizzaBundle/Notification/Strategies/SmsStrategy.php
@@ -9,6 +9,8 @@ use PizzaBundle\Repository\SmsNotificationRepository;
 
 class SmsStrategy implements SendingStrategy
 {
+    const TOKEN = "wygenerowany_token";
+
     /**
      * @var SmsNotificationRepository
      */
@@ -25,7 +27,44 @@ class SmsStrategy implements SendingStrategy
 
         /** @var SmsNotification $smsNotification */
         foreach ($smsNotifications as $smsNotification){
+            $params = array(
+                'to' => '500000000',
+                'from' => 'Info',
+                'message' => "Hello world!",
+            );
 
+            $this->sms_send($params, self::TOKEN);
         }
+    }
+
+    private function sms_send($params, $token, $backup = false)
+    {
+
+        if ($backup == true) {
+            $url = 'https://api2.smsapi.pl/sms.do';
+        } else {
+            $url = 'https://api.smsapi.pl/sms.do';
+        }
+
+        $c = curl_init();
+        curl_setopt($c, CURLOPT_URL, $url);
+        curl_setopt($c, CURLOPT_POST, true);
+        curl_setopt($c, CURLOPT_POSTFIELDS, $params);
+        curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($c, CURLOPT_HTTPHEADER, array(
+            "Authorization: Bearer $token"
+        ));
+
+        $content = curl_exec($c);
+        $http_status = curl_getinfo($c, CURLINFO_HTTP_CODE);
+
+        if ($http_status != 200 && $backup == false) {
+            $backup = true;
+            $this->sms_send($params, $token, $backup);
+        }
+
+        curl_close($c);
+
+        return $content;
     }
 }
